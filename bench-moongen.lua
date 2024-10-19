e3479c99550444653cc42f2212648878312ca33d
ramla <ijazramla@gmail.com>
2024-10-19 15:38:08 -0400

    undid change where latency packets have different ip addresses

--- bench-moongen.lua
+++ bench-moongen.lua
@@ -153,7 +153,7 @@ function _latencyTask(txQueue, rxQueue, layer, direction, flows, labels)
   local timestamper = ts:newUdpTimestamper(txQueue, rxQueue)
   local measureFunc = function()
     return timestamper:measureLatency(LATENCY_PACKETS_SIZE, function(buf)
-      packetInitsLatency[direction](buf, LATENCY_PACKETS_SIZE)
+      packetInits[direction](buf, LATENCY_PACKETS_SIZE)
       packetConfigs[direction][layer](buf:getUdpPacket(), counter)
       counter = (counter + 1) % flows
     end, 1) -- wait 1ms at most before declaring a packet lost, to avoid confusing old packets for new ones
